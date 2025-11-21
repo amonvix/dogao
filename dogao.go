3feb08f5db5a4112d25ad6f206c31051b640f3a6
amonvix <hspedroso@gmail.com>
2025-11-21 00:46:57 -0300

    first

---
+++ dogao.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("principal function")
+}
+
+func Latir() string {
+	return "WOOF!"
+}
+
+func Latidos() string {
+	return "WOOF! WOOF! WOOF!"
+}
